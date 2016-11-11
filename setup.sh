dac39cbd8add4a9e64e785dbe84bd936ae4884e3
gakky1667 <gakky@ubi.cs.ritsumei.ac.jp>
2016-11-11 17:23:37 +0900

    minor revision

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp ./vimrc/.vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
