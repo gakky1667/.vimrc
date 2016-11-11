ce549d14f188f3409a8bfba2eb020b16810da762
gakky1667 <gakky@ubi.cs.ritsumei.ac.jp>
2016-11-11 15:12:47 +0900

    Added start up script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp ./vimrc/.vimrc ~/.vimrc
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
+
+mkdir -p ~/.vim/colors
+cp ./molokai.vim ~/.vim/colors/molokai.vim
